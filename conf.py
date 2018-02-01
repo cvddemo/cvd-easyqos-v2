02d45f694a5d841f6bc4e7d1f95ce40445d5c8ba
cvddemo <35241310+cvddemo@users.noreply.github.com>
2018-01-31 16:21:25 -0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -58,11 +58,7 @@ gettext_compact = False
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = 'img/logo.svg'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
+
 
 def setup(app):
     app.add_stylesheet('stylesheet.css')
